3cb5df2a10963142e1a3deba7d5f822a91986cb0
aquispesegales <35657468+aquispesegales@users.noreply.github.com>
2025-02-23 22:16:46 -0400

    dasboard

--- src/components/HomePage/HomeComponent.vue
+++ src/components/HomePage/HomeComponent.vue
@@ -1,48 +1,90 @@
 <template>
-   <v-container>
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-card>
-              <v-card-title>Total Ventas</v-card-title>
-              <v-card-text>
-                <v-icon color="primary">mdi-currency-usd</v-icon>
-                <span class="text-h5">$ 50,000</span>
-              </v-card-text>
-            </v-card>
-          </v-col>
-          <v-col cols="12" md="4">
-            <v-card>
-              <v-card-title>Usuarios Activos</v-card-title>
-              <v-card-text>
-                <v-icon color="green">mdi-account-group</v-icon>
-                <span class="text-h5">1,200</span>
-              </v-card-text>
-            </v-card>
-          </v-col>
-          <v-col cols="12" md="4">
-            <v-card>
-              <v-card-title>Pedidos Pendientes</v-card-title>
-              <v-card-text>
-                <v-icon color="red">mdi-cart</v-icon>
-                <span class="text-h5">75</span>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
+  <v-container>
+    <v-row>
 
-        <v-row>
-          <v-col cols="12">
-            <v-card>
-              <v-card-title>Reporte de Ventas</v-card-title>
-              <v-card-text>
-                <BarChart />
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+      <v-col cols="12" md="4">
+        <v-card class="pa-4">
+          <v-card-title>Pagos por mensual del año {{ anioActual }}</v-card-title>
+          <v-card-text>
+            <v-table  density="compact">
+              <thead>
+                <tr>
+                  <th>Mes</th>
+                  <th>Total Pagado</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(pago, index) in lstPagosMes" :key="index">
+                  <td>{{ new Date(pago.mes).toLocaleDateString() }}</td>
+                  <td>{{ pago.total_pagado +' Bs' }}</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <v-card class="pa-4">
+          <v-card-title>Pagos semanal del mes de {{ nombreMes }} </v-card-title>
+          <v-card-text>
+            <v-table  density="compact">
+              <thead>
+                <tr>
+                  <th>Semana</th>
+                  <th>Total Pagado</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(pago, index) in lstPagosSemana" :key="index">
+                  <td>{{ new Date(pago.semana).toLocaleDateString() }}</td>
+                  <td>{{ pago.total_pagado +' Bs'}}</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-  //
+import { ref, defineProps, onMounted } from "vue";
+
+import { useLoadingStore } from '@/stores/useLoadingStore'
+const loadingStore = useLoadingStore()
+const $api = inject('api');
+
+onMounted(async () => {
+  console.log("montandooo");
+  await mostrarDashboard();
+});
+
+
+const nombreMes = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(new Date());
+const anioActual = new Date().getFullYear();
+
+
+const lstPagosMes = ref([]);
+const lstPagosSemana = ref([]);
+const lstPagosPorestado = ref([]);
+const lstPagosUltimo = ref([]);
+const mostrarDashboard = async () => {
+  loadingStore.startLoading('cargando indicadores....')
+  try {
+
+    const resPagoMes = await $api.get(`/transaction/pagos-mes`)
+    lstPagosMes.value = resPagoMes.data.result
+    const restPagoSemana = await $api.get(`/transaction/pagos-semana`)
+    lstPagosSemana.value = restPagoSemana.data.result
+
+    loadingStore.stopLoading()
+  } catch (error) {
+    console.log(error);
+    loadingStore.stopLoading()
+  }
+}
+
 </script>
