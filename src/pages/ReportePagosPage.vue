ede3b00e014b7a3379eb0ea6e259a5d5af63205f
aquispesegales <35657468+aquispesegales@users.noreply.github.com>
2025-05-14 16:32:55 -0400

    se ha quitado estado trasnaccion ya que no es neecsario

--- src/pages/ReportePagosPage.vue
+++ src/pages/ReportePagosPage.vue
@@ -39,7 +39,7 @@
             <td>CÓDIGO DEUDA</td>
             <td>METODO PAGO</td>
             <td>ENTIDAD COBRO</td>
-            <td>ESTADO TRANSACCIÓN</td>
+            <!--<td>ESTADO TRANSACCIÓN</td>-->
           </tr>
         </thead>
       </template>
@@ -62,7 +62,7 @@
           <td>{{ item.codigo_deuda }} </td>
           <td>{{ item.metodo_pago }}</td>
           <td>{{ item.entidad_cobro }}</td>
-          <td>{{ item.estado_transaccion	 }}</td>
+          <!--<td>{{ item.estado_transaccion	 }}</td>-->
         </tr>
       </template>
       <!-- 🔹 Pie de Tabla (SUMATORIA) -->
